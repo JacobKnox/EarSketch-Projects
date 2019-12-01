fbb8d2d9fa49d5b7a277af57fdd67eec2d8af8fe
Jacob Knox <epicdude203@gmail.com>
2019-12-01 10:18:11 -0800

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,16 @@
+# python code
+# script_name:
+#
+# author:
+# description:
+#
+
+from earsketch import *
+
+init()
+setTempo(120)
+
+fitMedia(RD_ELECTRO_DRUMROLLBREAK_6,1,1,5)
+fitMedia(ELECTRO_DRUM_MAIN_BEAT_002,2,5,10)
+
+finish()
